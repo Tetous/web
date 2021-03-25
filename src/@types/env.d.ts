fa8f85aa234e6d4518c515a579b3f7ea5d8632a6
Tetous <70217525+Tetous@users.noreply.github.com>
2021-03-25 13:13:44 -0700

    Update env.d.ts

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -2,6 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV: 'production' | 'development' | 'testing';
 
-    REACT_APP_API_URL: string;
+    https://animexninja-api-ksi4e4rox-tetous.vercel.app/api/: string;
   }
 }
